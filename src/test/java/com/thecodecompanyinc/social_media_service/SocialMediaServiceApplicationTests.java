7d1f3ca787fc810ed56cb911fd08f854e3f35b7f
zkrallah <muhammad.heshamyt@gmail.com>
2026-08-27 16:37:46 +0300

    Init Spring Boot project.

---
+++ src/test/java/com/thecodecompanyinc/social_media_service/SocialMediaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thecodecompanyinc.social_media_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMediaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
